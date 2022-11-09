577adc89a2eff72f2b2cbbc45ba8c472e43e9394
Lucas Malatchoumy <malatchoumylucas.com>
2022-11-10 00:54:25 +0100

    fix(cors): enabled cors

--- server/app.js
+++ server/app.js
@@ -1,17 +1,25 @@
-const express = require('express');
-
+const express = require("express");
+const cors = require("cors");
 const hostname = "0.0.0.0";
 const port = 3001;
 
 const server = express();
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded());
+server.use((_, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 const mongoose = require("mongoose");
 // mongoose.connect("mongodb://localhost:27017/apinode"); // Whithout Docker
 mongoose.connect("mongodb://mongo/apinode");
-
-server.use(express.urlencoded());
-server.use(express.json());
-
 
 // const postRoute = require("./api/routes/postRoute");
 // postRoute(server);
