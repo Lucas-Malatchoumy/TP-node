487ebbb7f111a5844cbb29e904b6dafbddb9d4e8
Lucas Malatchoumy <malatchoumylucas.com>
2022-11-10 00:10:00 +0100

    feat(Login): create bootstrap Login form

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,54 @@
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+//import { addUser } from '../api/services/user';
+import Form from 'react-bootstrap/Form';
+import { useQueryClient, useMutation } from 'react-query';
+import { useForm, Controller } from "react-hook-form";
+import { useNavigate } from 'react-router-dom';
+
+export default function NewUser() {
+    const addUser = () => console.log('hey');
+    const queryClient = useQueryClient();
+    const navigate = useNavigate();
+    const { mutateAsync, isSuccess } = useMutation(addUser);
+    const { control, handleSubmit } = useForm({
+        defaultValues: {
+          email: '',
+          password: '',
+        }
+      });
+      const onSubmit = async (data) => {
+        // await mutateAsync(data)
+        alert('hello')
+      };
+    //   if (isSuccess) {
+    //     queryClient.invalidateQueries('usersData')
+    //     setTimeout(() => {
+    //         navigate("/")
+    //       }, 1000)
+    //   }
+  return (
+    <Form className='w-25 mx-auto' onSubmit={handleSubmit(onSubmit)}>
+      <Row className='mb-3'>
+        <Form.Group as={Col} controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Controller 
+            render={({field}) => <Form.Control type="email" placeholder="Email"{...field}/>}
+            name="email"
+            control={control} />
+        </Form.Group>
+        <Form.Group as={Col} controlId="formBasicPassword">
+            <Form.Label>Mot de passe</Form.Label>
+            <Controller 
+            render={({field}) => <Form.Control type="password" placeholder="Mot de passe"{...field}/>}
+            name="password"
+            control={control} />
+        </Form.Group>
+      </Row>
+      <Button className='mt-3' variant="success" type="submit">
+        Envoyer
+      </Button>
+    </Form>
+  );
+}
