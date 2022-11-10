87de625253319cda6b8ba71ca2124d50634dff0c
Lucas Malatchoumy <malatchoumylucas.com>
2022-11-10 18:30:12 +0100

    feat(Home): retreive all created post

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import { Card } from "react-bootstrap";
 import { Button } from "react-bootstrap";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
+import { getPosts } from "../services/post";
 
 export default function BasicExample() {
   const navigate = useNavigate();
+  const { error, data } = useQuery("posts", getPosts);
+  const posts = data || [];
+  if (error) {
+    alert("heyyyyy");
+  }
   const { mutateAsync, isLoading } = useMutation(() =>
     localStorage.removeItem("token")
   );
@@ -14,17 +20,17 @@ export default function BasicExample() {
     navigate("/login");
   };
   return (
-    <>
-      <Card border="primary" style={{ width: "18rem" }}>
-        <Card.Header>Header</Card.Header>
-        <Card.Body>
-          <Card.Title>Primary Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+    <div>
+      {posts.map((post) => {
+        return (
+          <Card border="primary" style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Primary Card Title</Card.Title>
+              <Card.Text>{post.content}</Card.Text>
+            </Card.Body>
+          </Card>
+        );
+      })}
       <br />
       <Button
         disabled={isLoading}
@@ -34,6 +40,6 @@ export default function BasicExample() {
       >
         {"Supprimer"}
       </Button>
-    </>
+    </div>
   );
 }
